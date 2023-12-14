51a82586a054f736c633cf5adc71d61d2edce1ed
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2023-12-14 13:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+function fizzbuzz(i){
+    if(i%3==0 && i%5==0){
+        return("FizzBuzz")
+    }
+    else if(i%3==0){
+        return ("Fizz")
+    }
+    else if(i%5==0){
+        return ("Bizz")
+    }
+    else{
+        return i;
+    }
+}
+
+let start=0;
+let end=100;
+for (let i=start;i<=end;i++){
+    //alert(fizzbuzz(i));
+}
